db41df6faf11cf334ab811991f1c1e5a3a630e66
Deymoslor <dylanmurillo.rios.inead@gmail.com>
2022-02-17 18:40:51 -0500

    primer commit eventos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
+    EventsComponent,
   ],
   imports: [
     BrowserModule,
