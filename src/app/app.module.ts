3b64d332499709443f4c8fec70721ec174aaf533
Deymoslor <dylanmurillo.rios.inead@gmail.com>
2022-02-20 22:43:55 -0500

    Limpiando para bootstrap y para probar el cambio a sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EventsComponent } from './events/events.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -24,15 +17,7 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule,
     BrowserAnimationsModule,
-    MatRadioModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    MatCheckboxModule,
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-events',
@@ -9,21 +7,13 @@ import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 })
 
 export class EventsComponent implements OnInit {
-  model!: NgbDateStruct;
   checked = false;
-  date!: {year: number, month: number};
   seasons: string[] = ['Presencial', 'Virtual'];
-  constructor(private modalService: NgbModal, private calendar: NgbCalendar) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  selectToday() {
-    this.model = this.calendar.getToday();
-  }
-  
-  openScrollableContent(longContent:any) {
-    this.modalService.open(longContent, { scrollable: true, size: 'xl'});
-  }
+
 
 }
