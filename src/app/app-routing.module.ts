300b20b3e066a7300dfa11145003531ded25df27
Deymoslor <dylanmurillo.rios.inead@gmail.com>
2022-02-23 15:30:24 -0500

    Creando componente base para el dashboar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EventsComponent } from './events/events.component';
+import { DashboardComponent } from './events/dashboard/dashboard.component';
 
 const routes: Routes = [
-  {path: 'events', component:EventsComponent}
+  {path: 'events', component:EventsComponent},
+  {path: 'dashboard', component:DashboardComponent}
 ];
 
 @NgModule({
