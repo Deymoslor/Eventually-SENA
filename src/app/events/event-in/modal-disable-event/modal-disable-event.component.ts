dad42bb34da3ff60714555b721a1755a7f5ea3f0
Deymoslor <dylanmurillo.rios.inead@gmail.com>
2022-02-26 04:29:02 -0500

    Terminando lo que parece ser 4.

---
+++ src/app/events/event-in/modal-disable-event/modal-disable-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-disable-event',
+  templateUrl: './modal-disable-event.component.html',
+  styleUrls: ['./modal-disable-event.component.scss']
+})
+export class ModalDisableEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -8,13 +8,15 @@ import { EventInComponent } from './event-in/event-in.component';
 import { EventsRoutingModule } from './events-routing.module';
 import { ModalResultComponent } from './event-in/modal-result/modal-result.component';
 import { ModalEditComponent } from './event-in/modal-edit/modal-edit.component';
+import { ModalDisableEventComponent } from './event-in/modal-disable-event/modal-disable-event.component';
 
 @NgModule({
   declarations: [EventsComponent,
     ModalCreateEventComponent,
     EventInComponent,
     ModalResultComponent,
-    ModalEditComponent,],
+    ModalEditComponent,
+    ModalDisableEventComponent,],
   imports: [
     CommonModule,
     NgbModule,
