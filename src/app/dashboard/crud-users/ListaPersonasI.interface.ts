7c72621ec087185a4a295eb960b305bd6483b081
jordan-Florez <jordansaldarriaga14@gmail.com>
2022-03-16 20:54:57 -0500

    Pasando código de crud-users

---
+++ src/app/dashboard/crud-users/ListaPersonasI.interface.ts
@@ -0,0 +1,17 @@
+export interface ListaPersonasI{
+
+    idPersona:string;
+
+    nombre:string;
+
+    apellidos:string;
+
+    documento:string;
+
+    fechaNacimiento:string;
+
+    Email:string;
+
+    Estado:string;
+
+}
---
+++ src/app/dashboard/crud-users/service/user-service.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { ListaPersonasI } from '../ListaPersonasI.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+  //Definimos ruta raíz de el API.
+
+  API:string='http://localhost/API_rest_Eventually/';
+
+  //Definimos dentro del constructor el cliente http.
+  constructor(
+
+    //Inyectamos el HttpClient.
+    private http:HttpClient
+
+  ) { }
+
+  //Creamos método para obtener pacientes.
+  getAllPersons(page:number):Observable<ListaPersonasI[]>{
+
+  //Creamos una variable con la dirección de envío de información.
+  let direccion = this.API + "persons?page=" + page;
+
+  //Creamos el retorno de un método get que recibirá datos del tipo de la interfaz.
+  return this.http.get<ListaPersonasI[]>(direccion);
+
+  }
+
+}
