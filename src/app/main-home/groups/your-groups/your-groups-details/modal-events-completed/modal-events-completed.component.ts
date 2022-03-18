9f900916af539ace6721e4e0a084016ad1e44150
elpastelito <sacuna66@misena.edu.co>
2022-03-18 16:56:05 -0500

    subiendo actualización del detalles grupo

---
+++ src/app/main-home/groups/your-groups/your-groups-details/modal-events-completed/modal-events-completed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-events-completed',
+  templateUrl: './modal-events-completed.component.html',
+  styleUrls: ['./modal-events-completed.component.scss']
+})
+export class ModalEventsCompletedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
