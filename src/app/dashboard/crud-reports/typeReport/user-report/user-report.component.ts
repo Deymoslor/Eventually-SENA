efd082c764d0034a56d6bc9627c802488b1ee92c
unknown <kesan0309@gmail.com>
2022-04-07 02:36:17 -0500

    reportes de hultima hora

--- src/app/dashboard/crud-reports/typeReport/user-report/user-report.component.ts
+++ src/app/dashboard/crud-reports/typeReport/user-report/user-report.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+// import {jsPDF} from 'node_modules/jspdf';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-user-report',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserReportComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+  }
+
+  createPDF(){
+
+    const pdfDefinition: any = {
+      content: [
+        {
+          layout: 'lightHorizontalLines', // optional
+          table: {
+          // headers are automatically repeated if the table spans over multiple pages
+          // you can declare how many rows should be treated as headers
+          headerRows: 1,
+          widths: [ '*', 'auto', 100, '*' ],
+
+          body: [
+            [ 'First', 'Second', 'Third', 'The last one' ],
+            [ 'Value 1', 'Value 2', 'Value 3', 'Value 4' ],
+            [ { text: 'Bold value', bold: true }, 'Val 2', 'Val 3', 'Val 4' ]
+          ]
+        }
+        }
+      ]
+    }
+
+    const pdf = pdfMake.createPdf(pdfDefinition);
+    pdf.open();
+
   }
 
 }
