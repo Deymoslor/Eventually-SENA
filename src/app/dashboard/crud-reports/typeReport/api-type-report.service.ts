71dba6ddb2d4753c6a0742206e3a64cdba024d98
unknown <kesan0309@gmail.com>
2022-04-07 08:29:42 -0500

    cuando te veo

--- src/app/dashboard/crud-reports/typeReport/api-type-report.service.ts
+++ src/app/dashboard/crud-reports/typeReport/api-type-report.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Groups } from 'src/app/main-home/groups/see-groups/groups';
+import { ListEventsI } from 'src/app/models/listEvents.interface';
+import { ListGroups } from '../../crud-groups/listGroups.interface';
+import { ListaPersonasI } from '../../crud-users/ListaPersonasI.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiTypeReportService {
 
-  constructor() { }
+  API:string='http://localhost/angularProyectos/Api-Eventually-SENA/';
+
+  constructor(private http:HttpClient) { }
+
+  getAllPersons(page:number):Observable<ListaPersonasI[]>{
+
+    let direccion = this.API + "persons?page=" + page;
+
+    return this.http.get<ListaPersonasI[]>(direccion);
+
+    }
+
+  getAllGroups(page:number):Observable<ListGroups[]>{
+
+    let direccion = this.API + "CRUDGroups?page=" + page;
+
+    return this.http.get<ListGroups[]>(direccion);
+
+  }
+
+  getAllEvents(page:number):Observable<ListEventsI[]>{
+
+    let dir = this.API + "eventos?page=" + page;
+
+    return this.http.get<ListEventsI[]>(dir);
+
+  }
 }
--- src/app/dashboard/crud-reports/typeReport/event-report/event-report.component.ts
+++ src/app/dashboard/crud-reports/typeReport/event-report/event-report.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import html2canvas from 'html2canvas';
+import { ListEventsI } from 'src/app/models/listEvents.interface';
+import { ApiTypeReportService } from '../api-type-report.service';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-event-report',
   templateUrl: './event-report.component.html',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventReportComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+
+  events?:ListEventsI[];
+
+  constructor(private api:ApiTypeReportService) { }
 
   ngOnInit(): void {
+    this.api.getAllEvents(1).subscribe(data =>{
+      this.events = data;
+    })
+  }
+
+  createPDFEvent(){
+
+    html2canvas(document.getElementById('PdfEventos')!).then(function (canvas) {
+      var data = canvas.toDataURL();
+      var pdfDefinition = {
+          content: [{
+              image: data,
+              width: 500,
+          }]
+      };
+      pdfMake.createPdf(pdfDefinition).download("Reportes_Eventos.pdf");
+  });
+
   }
 
 }
