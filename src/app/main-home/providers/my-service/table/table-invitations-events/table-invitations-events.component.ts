14b3945b3f95bed6d012014d0e2085a82c8a502c
Deymoslor <dylanmurillo.rios.inead@gmail.com>
2022-04-13 14:34:16 -0500

    Detalle invitacion a evento - servicio

--- src/app/main-home/providers/my-service/table/table-invitations-events/table-invitations-events.component.ts
+++ src/app/main-home/providers/my-service/table/table-invitations-events/table-invitations-events.component.ts
@@ -10,7 +10,7 @@ import { ApiService } from '../../../services/api.service';
 })
 export class TableInvitationsEventsComponent implements OnInit {
 
-
+  accepted: boolean = false;
   dataServiceEventProv!: ServiceEventI[];
 
   constructor(private api: ApiService) { }
@@ -18,6 +18,11 @@ export class TableInvitationsEventsComponent implements OnInit {
   ngOnInit(): void {
     this.api.getAllServiceEventProv(1).subscribe(data =>{
       console.log(data);
+      data.forEach(key => {
+        if (key.estadoInvitacion == 1) {
+          this.accepted = true;
+        }
+      });
       this.dataServiceEventProv = data;
     })
   }
@@ -28,7 +33,7 @@ export class TableInvitationsEventsComponent implements OnInit {
     this.api.putServiceEventProv(form).subscribe(data=>{
       console.log(data);
     });
-   
+    this.refresh();
 }
 
   changeStateServiceProvD(form:ServiceEventI){
