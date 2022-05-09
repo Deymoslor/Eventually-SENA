362b571fa7cab75991622d796b23ca5809b86881
unknown <kesan0309@gmail.com>
2022-05-09 11:11:52 -0500

    arreglitos de grupos act ll

--- src/app/main-home/groups/see-groups/see-group-detail/see-group-detail.component.ts
+++ src/app/main-home/groups/see-groups/see-group-detail/see-group-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/core/service/auth.service';
 import { Group } from '../group';
 import { SeeGroupsService } from '../see-groups.service';
 
@@ -15,10 +16,12 @@ export class SeeGroupDetailComponent implements OnInit {
   constructor(
     private SeeGroupsService: SeeGroupsService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private auth: AuthService,
     ) { }
 
     group!: Group;
+    idPersonas = this.auth.desencriptar(localStorage.getItem('id'));
 
   ngOnInit(): void {
     // const idGrupos = this.route.snapshot.paramMap.get('id');
@@ -46,6 +49,21 @@ export class SeeGroupDetailComponent implements OnInit {
 
     // }
 
+  }
+
+  share(): void {
+    console.log(' la variable idGrupos: ', this.group.idGrupos);
+    console.log(' la variable idPersonas: ', this.idPersonas);
+    // console.log(this.GroupPersonC.idGrupos);
+    // this.GroupPersonC.idGrupos = this.group.idGrupos;
+    // this.GroupPersonC.idPersonas = Number(this.idPersonas);
+    // console.log(this.GroupPersonC);
+    const newDetail = {idGrupos: this.group.idGrupos, idPersonas: Number(this.idPersonas)}
+    this.SeeGroupsService.postDetailsGroupPerson(newDetail).subscribe(data =>
+      console.log(data)
+
+    );
+    window.alert('se ha enviado la solicitúd de unirse');
   }
 
 }
