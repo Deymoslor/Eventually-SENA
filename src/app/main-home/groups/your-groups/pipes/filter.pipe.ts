19d00986241c5ca8ac828e98cf1e39c4087c1334
elpastelito <kesan0309@gmail.com>
2022-05-10 12:38:31 -0500

    intento tabla

---
+++ src/app/main-home/groups/your-groups/pipes/filter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter'
+})
+export class FilterPipe implements PipeTransform {
+
+  transform(value: any, arg: any): any {
+    if (arg === '' || arg.length) return value;
+    const resultPersonas = [];
+    for (const persona of value) {
+      if (persona.Email.toLowerCase().indexOf(arg.toLoweCase()) > -1) {
+        console.log('si');
+        // resultPersonas.push(persona);
+      }
+
+    }
+    // return resultPersonas;
+  }
+
+}
