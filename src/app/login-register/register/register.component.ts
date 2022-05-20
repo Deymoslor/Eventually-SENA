324058c3df2947fc78376ca228988265060d556f
jordan-Florez <jordansaldarriaga14@gmail.com>
2022-05-19 20:12:18 -0500

    Subiendo cambios para hacer checkout, solo información relacionada al perfil en los cambios

--- src/app/login-register/register/register.component.ts
+++ src/app/login-register/register/register.component.ts
@@ -38,17 +38,17 @@ export class RegisterComponent implements OnInit {
       // })
 
       this.nuevoForm = this.formBuilder.group({
-        nombre: ['', [Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]],
-        apellidos: ['', [Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]],
+        nombre: ['', [Validators.required, Validators.pattern(/^[a-zA-ZÀ-ÿ\u00f1\u00d1 ]+$/)]],
+        apellidos: ['', [Validators.required, Validators.pattern(/^[a-zA-ZÀ-ÿ\u00f1\u00d1 ]+$/)]],
         // documento: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(10)]],
-        documento: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(8), Validators.pattern(/^[0-9]\d*$/)]],
+        documento: ['', [Validators.required, Validators.minLength(7), Validators.maxLength(10), Validators.pattern(/^[0-9]\d*$/)]],
         fechaNacimiento: ['',Validators.required],
         Email: ['',[Validators.required, Validators.email]],
         password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(12)]],
         Celular: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10), Validators.pattern(/^[0-9]\d*$/)]],
-        ciudad: ['', [Validators.required, Validators.pattern(/^[a-zA-Z ]+$/)]],
+        ciudad: ['', [Validators.required, Validators.pattern(/^[a-zA-ZÀ-ÿ\u00f1\u00d1 ]+$/)]],
         check1: ['', Validators.requiredTrue],
-        check2: ['', Validators.requiredTrue],
+        // check2: ['', Validators.requiredTrue],
       })
 
     }
@@ -70,57 +70,54 @@ export class RegisterComponent implements OnInit {
     //Log para revisar los datos del formulario.
     // console.log(form);
     let dateU = form.fechaNacimiento;
-    console.log('fecha usuario: ' + dateU);
+    // console.log('fecha usuario: ' + dateU);
 
 
     let yearU = parseInt(dateU.substring(0,4));
     let monthU = parseInt(dateU.substring(5,7));
     let dayU = parseInt(dateU.substring(8,10));
-    console.log('Año Usuario: ' + yearU);
-    console.log('Mes Usuario: ' + monthU);
-    console.log('Dia Usuario: ' + dayU);
+    // console.log('Año Usuario: ' + yearU);
+    // console.log('Mes Usuario: ' + monthU);
+    // console.log('Dia Usuario: ' + dayU);
 
     let date = new Date();
     let fullDate = date.getDate();
     let yearS = date.getFullYear();
-    let monthS = date.getMonth();
+    let monthS = date.getMonth() + 1;
     let dayS = date.getDate();
-    console.log('Fecha Sistema: ' + fullDate);
-    console.log('Año sistema: ' + yearS);
-    console.log('Mes sistema: ' + monthS);
-    console.log('Dia Sistema: ' + fullDate);
+    // console.log('Fecha Sistema: ' + fullDate);
+    // console.log('Año sistema: ' + yearS);
+    // console.log('Mes sistema: ' + monthS);
+    // console.log('Dia Sistema: ' + fullDate);
 
     // if ((yearS - yearU) >= 14 && (monthS <= monthU)) {
-    if ((yearS - yearU) >= 14) {
-      console.log('entrando aquí');
+    if ((yearS - yearU) >= 18) {
+      // console.log('entrando aquí');
       // this.lawValidator = true;
       // lawValidatorFunc();
-      if ((yearS - yearU) >= 14 && (yearS - yearU) <= 17) {
-        this.lawValidator = true;
-        console.log(this.lawValidator);
-      }else{
-        this.lawValidator = false;
-        console.log(this.lawValidator);
-      }
+      // if ((yearS - yearU) >= 14 && (yearS - yearU) <= 17) {
+      //   this.lawValidator = true;
+      //   console.log(this.lawValidator);
+      // }else{
+      //   this.lawValidator = false;
+      //   console.log(this.lawValidator);
+      // }
 
-      console.log(this.nuevoForm.get('check2'));
-      
+      // if (this.nuevoForm.get('check2')) {
+        this.service.postUser(form).subscribe(data =>{
+          console.log(data);
+          this.router.navigate(['/loginRegister/login']);
+        });
+      // }
+      // console.log('Primera condi');
 
-      if (this.nuevoForm.get('check2')) {
-        // this.service.postUser(form).subscribe(data =>{
-        //   // console.log(data);
-        //   this.router.navigate(['/loginRegister']);
-        // });
-      }
-    }else if((yearS - yearU) == 13 && monthS == monthU && dayS == dayU){
-      console.log("Cumple años este día");
-      
+    }else if((yearS - yearU) == 17 && monthS == monthU && dayS == dayU){
+      this.service.postUser(form).subscribe(data =>{
+        console.log(data);
+        this.router.navigate(['/loginRegister/login']);
+      });
+      // console.log("Cumple años este día");
     }
-    // window.location.reload();
-  }
-
-  showLaw(){
-        
   }
 
   // lawValidatorFunc(yearS:number,yearU:number,monthS:number,monthU:number){
