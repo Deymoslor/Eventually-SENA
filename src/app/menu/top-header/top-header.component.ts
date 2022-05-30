a8d4a5c3098a9e36f8e97e898049b7d3f3596735
jordan-Florez <jordansaldarriaga14@gmail.com>
2022-05-29 22:14:04 -0500

    funcionando el ocultar el botón de perfil del header para el usuario proveedor, además de traer el nombre de la persona o proveedor. En la API cambio sobre verificación al registrarse de si el correo está siendo usado, además encriptando código del proveedor y enviando correo con credenciales de inicio de sesión

--- src/app/menu/top-header/top-header.component.ts
+++ src/app/menu/top-header/top-header.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { windowWhen } from 'rxjs';
+import { AuthService } from '../../core/service/auth.service';
+import { UpdateServiceService } from '../../main-home/settings/settingsService/update-service.service';
+import { SupplierService } from '../../dashboard/crud-suppliers/service/supplier.service';
 
 @Component({
   selector: 'app-top-header',
@@ -9,11 +12,36 @@ import { windowWhen } from 'rxjs';
 })
 export class TopHeaderComponent implements OnInit {
 
+  public rol!:string;
+  public nombrePerfil:any = undefined;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private authService: AuthService,
+    private updateService: UpdateServiceService,
+    private supplierService: SupplierService
   ) { }
 
   ngOnInit(): void {
+
+    //Desencriptamos el rol para saber si mostrar o no algunas cosas.
+    this.rol = this.authService.desencriptar(localStorage.getItem('nombreRol'));
+
+    //Condición para saber si viene vacío el nombre como invitado.
+    if(this.rol != 'PROVEEDOR'){
+      //Buscamos si es invitado para traer su nombre, en caso de venir vacío buscamos en proveedor.
+      this.updateService.getSinglePerson(this.authService.desencriptar(localStorage.getItem('id'))).subscribe((data:any) =>{
+        let datosPersona = data[0];
+        this.nombrePerfil = datosPersona.nombre;
+      });
+    }else{
+      //Buscamos si es proveedor para traer su nombrem en caso de venir vacío buscamos en proveedor.
+      this.supplierService.getSingleSupplier(this.authService.desencriptar(localStorage.getItem('id'))).subscribe((data:any) =>{
+        let datosProveedor = data[0];
+        this.nombrePerfil = datosProveedor.nombreProveedor;
+      });
+    }
+
   }
 
   logOut(){
