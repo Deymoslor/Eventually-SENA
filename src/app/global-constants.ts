16c29cbf57b7fb509bd33494ac04436222c66467
jordan-Florez <jordansaldarriaga14@gmail.com>
2022-06-07 17:31:37 -0500

    Seguir trabajando en la casa

--- src/app/global-constants.ts
+++ src/app/global-constants.ts
@@ -1,6 +1,6 @@
 export class GlobalConstants {
-  // public static port: string = ":8181" //SENA
-  public static port: string = "" //CASA
+  public static port: string = ":8181" //SENA
+  //public static port: string = "" //CASA
 
   // public static httpLocalHost: string 'http://localhost:8181'; //SENA
   public static httpLocalHost: string  = 'http://localhost'; //CASA
