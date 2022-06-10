00e1306da2432ce0d4ce0a2d225758e3502607a8
Deymoslor <dylanmurillo.rios.inead@gmail.com>
2022-06-09 23:03:19 -0500

    Trabajo si terminar de dashboard servicios. 09-06

--- src/app/dashboard/crud-services/crud-services.component.ts
+++ src/app/dashboard/crud-services/crud-services.component.ts
@@ -5,6 +5,9 @@ import { Router } from '@angular/router';
 import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ServiceI } from './models/services.interface';
+import { SupplierService } from '../crud-suppliers/service/supplier.service';
+import { ProveedorI } from '../crud-suppliers/modal-suppliers-create/ProveedorI.interface';
+import { ListaProveedoresI } from '../crud-suppliers/ListaProveedoresI.interface';
 
 @Component({
   selector: 'app-crud-services',
@@ -14,14 +17,14 @@ import { ServiceI } from './models/services.interface';
 export class CrudServicesComponent implements OnInit {
 
   typeServicesForm = new FormGroup({
-    idtipoServicio : new FormControl(''),
+    idtipoServicio: new FormControl(''),
     tipoServicio: new FormControl(''),
     estadoTipoServicio: new FormControl('')
   })
 
   ServicesForm = new FormGroup({
-    idServicios: new FormControl(), 
-    nombreServicio : new FormControl(''),
+    idServicios: new FormControl(),
+    nombreServicio: new FormControl(''),
     descripcionServicio: new FormControl(''),
     precioEstimado: new FormControl(),
     imagen: new FormControl(''),
@@ -35,24 +38,32 @@ export class CrudServicesComponent implements OnInit {
 
   dataType!: TypeServicesI;
   dataService!: ServiceI;
-  idTipo?:number;
+  idTipo?: number;
+
+  dataProvider!: ListaProveedoresI[];
 
   TypeServices!: TypeServicesI[];
   closeResult!: string;
 
   Services!: ServiceI[];
 
-  constructor(private api:ApiService, private router:Router, private modalService: NgbModal) { }
+  constructor(private api: ApiService, private router: Router, private modalService: NgbModal,
+    private apiProvider: SupplierService) { }
 
   ngOnInit(): void {
-    this.api.getAllTypeServices(1).subscribe(data =>{
-      console.log(data);
+    this.api.getAllTypeServices(1).subscribe(data => {
+      // console.log(data);
       this.TypeServices = data;
     })
 
-    this.api.getAllServices(1).subscribe(data =>{
-      console.log(data);
+    this.api.getAllServices(1).subscribe(data => {
+      // console.log(data);
       this.Services = data;
+    })
+
+    this.apiProvider.getAllSuppliers(1).subscribe(data =>{
+      // console.log(data);
+      this.dataProvider =data;
     })
 
   }
@@ -61,52 +72,52 @@ export class CrudServicesComponent implements OnInit {
     console.log('chica que dice');
   }
 
-  createEvent(){
-      this.router.navigate(['dashboard/createTypeServices']);
-      console.log(this.router)
+  createEvent() {
+    this.router.navigate(['dashboard/createTypeServices']);
+    console.log(this.router)
   }
 
   //MODAL
-  modalTypeServiceOpen(content:any, numb:number){
+  modalTypeServiceOpen(content: any, numb: number) {
     this.idTipo = numb;
-    this.api.getSingleTypeService(this.idTipo).subscribe((data:any) =>{
+    this.api.getSingleTypeService(this.idTipo).subscribe((data: any) => {
       this.dataType = data[0];
       console.log(this.dataType);
       this.typeServicesForm.setValue({
-        'idtipoServicio': this.dataType.idtipoServicio ,
+        'idtipoServicio': this.dataType.idtipoServicio,
         'tipoServicio': this.dataType.tipoServicio,
         'estadoTipoServicio': this.dataType.estadoTipoServicio
       })
     })
-    
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result: any) => {
+
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result: any) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason: any) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
 
-  modalServiceOpen(content:any, numb:number){
+  modalServiceOpen(content: any, numb: number) {
     this.idTipo = numb;
-    this.api.getSingleService(this.idTipo).subscribe((data:any) =>{
+    this.api.getSingleService(this.idTipo).subscribe((data: any) => {
       this.dataService = data[0];
       console.log(this.dataService.TipoServicio_idtipoServicio)
       this.ServicesForm.setValue({
-          'idServicios': this.dataService.idServicios,
-          'nombreServicio': this.dataService.nombreServicio,
-          'descripcionServicio': this.dataService.descripcionServicio,
-          'precioEstimado': this.dataService.precioEstimado,
-          'imagen': this.dataService.imagen,
-          'historialEmpresas': this.dataService.historialEmpresas,
-          'numeroContacto': this.dataService.numeroContacto,
-          'correoContacto': this.dataService.correoContacto,
-          'estadoServicio': this.dataService.estadoServicio,
-          'Proveedor_idProveedor': this.dataService.Proveedor_idProveedor,
-          'TipoServicio_idtipoServicio': this.dataService.TipoServicio_idtipoServicio,
+        'idServicios': this.dataService.idServicios,
+        'nombreServicio': this.dataService.nombreServicio,
+        'descripcionServicio': this.dataService.descripcionServicio,
+        'precioEstimado': this.dataService.precioEstimado,
+        'imagen': this.dataService.imagen,
+        'historialEmpresas': this.dataService.historialEmpresas,
+        'numeroContacto': this.dataService.numeroContacto,
+        'correoContacto': this.dataService.correoContacto,
+        'estadoServicio': this.dataService.estadoServicio,
+        'Proveedor_idProveedor': this.dataService.Proveedor_idProveedor,
+        'TipoServicio_idtipoServicio': this.dataService.TipoServicio_idtipoServicio,
       })
     })
-    
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result: any) => {
+
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result: any) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason: any) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
@@ -123,41 +134,41 @@ export class CrudServicesComponent implements OnInit {
     }
   }
 
-  switchStateEvent(num: number){
-    if(num != 1){
+  switchStateEvent(num: number) {
+    if (num != 1) {
       console.log("hola soy el num " + num);
       this.typeServicesForm.setValue({
-          'idtipoServicio': this.dataType.idtipoServicio,
-          'tipoServicio': this.dataType.tipoServicio,
-          'estadoTipoServicio': 1
+        'idtipoServicio': this.dataType.idtipoServicio,
+        'tipoServicio': this.dataType.tipoServicio,
+        'estadoTipoServicio': 1
       })
-    }else if (num == 1) {
+    } else if (num == 1) {
       console.log("hola soy el num " + num);
       this.typeServicesForm.setValue({
-          'idtipoServicio': this.dataType.idtipoServicio,
-          'tipoServicio': this.dataType.tipoServicio,
-          'estadoTipoServicio': 0
+        'idtipoServicio': this.dataType.idtipoServicio,
+        'tipoServicio': this.dataType.tipoServicio,
+        'estadoTipoServicio': 0
       })
-    } 
+    }
   }
 
-  switchStateService(num: number){
-    if(num != 1){
+  switchStateService(num: number) {
+    if (num != 1) {
       console.log("hola soy el num " + num);
       this.ServicesForm.setValue({
-          'idServicios': this.dataService.idServicios,
-          'nombreServicio': this.dataService.nombreServicio,
-          'descripcionServicio': this.dataService.descripcionServicio,
-          'precioEstimado': this.dataService.precioEstimado,
-          'imagen': this.dataService.imagen,
-          'historialEmpresas': this.dataService.historialEmpresas,
-          'numeroContacto': this.dataService.numeroContacto,
-          'correoContacto': this.dataService.correoContacto,
-          'estadoServicio': 1,
-          'Proveedor_idProveedor': this.dataService.Proveedor_idProveedor,
-          'TipoServicio_idtipoServicio': this.dataService.TipoServicio_idtipoServicio,
+        'idServicios': this.dataService.idServicios,
+        'nombreServicio': this.dataService.nombreServicio,
+        'descripcionServicio': this.dataService.descripcionServicio,
+        'precioEstimado': this.dataService.precioEstimado,
+        'imagen': this.dataService.imagen,
+        'historialEmpresas': this.dataService.historialEmpresas,
+        'numeroContacto': this.dataService.numeroContacto,
+        'correoContacto': this.dataService.correoContacto,
+        'estadoServicio': 1,
+        'Proveedor_idProveedor': this.dataService.Proveedor_idProveedor,
+        'TipoServicio_idtipoServicio': this.dataService.TipoServicio_idtipoServicio,
       })
-    }else if (num == 1) {
+    } else if (num == 1) {
       console.log("hola soy el num " + num);
       this.ServicesForm.setValue({
         'idServicios': this.dataService.idServicios,
@@ -172,20 +183,20 @@ export class CrudServicesComponent implements OnInit {
         'Proveedor_idProveedor': this.dataService.Proveedor_idProveedor,
         'TipoServicio_idtipoServicio': this.dataService.TipoServicio_idtipoServicio,
       })
-    } 
+    }
   }
 
-  postEditFormType(form: TypeServicesI){
+  postEditFormType(form: TypeServicesI) {
     console.log(form);
-    this.api.putTypeService(form).subscribe(data=>{
+    this.api.putTypeService(form).subscribe(data => {
       console.log(data);
     });
     this.refresh();
   }
 
-  postEditFormServices(form: ServiceI){
+  postEditFormServices(form: ServiceI) {
     console.log(form);
-    this.api.putService(form).subscribe(data=>{
+    this.api.putService(form).subscribe(data => {
       console.log(data);
     });
     this.refresh();
