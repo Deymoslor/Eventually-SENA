b5fc54c588e104a512258cdcc818e6c446009b13
Orlon05 <oscarandresrolonmeneses@gmail.com>
2022-06-11 22:58:34 -0500

    Penultimos retoques responsive en dashboard

--- src/app/menu/user-menu/user-menu.component.ts
+++ src/app/menu/user-menu/user-menu.component.ts
@@ -59,8 +59,8 @@ export class UserMenuComponent{
         this.nombrePerfil = this.datosPersona.nombre;
 
         this.imagenPerfilForm.setValue({
-          // 'imagen': this.imagenProfile.imagen.replace('C:/xampp/htdocs', this.httpLocalHost),
-          'imagen' : this.imagenProfile.imagen.replace('J:/Programas/Xampp/htdocs', this.httpLocalHost),
+          'imagen': this.imagenProfile.imagen.replace('C:/xampp/htdocs', this.httpLocalHost),
+          // 'imagen' : this.imagenProfile.imagen.replace('J:/Programas/Xampp/htdocs', this.httpLocalHost),
         })
       });
     }else{
